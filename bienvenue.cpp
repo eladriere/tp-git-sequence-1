d2b9101a5c7b2e6869655cc8a7494f0de3e4c40a
ladriere <enzo.ladriere.pro@gmail.com>
2021-09-16 15:13:21 +0200

    Rajout de bienvenue.cpp

--- bienvenue.cpp
+++ bienvenue.cpp
@@ -1,10 +1,10 @@
 // Affiche un message de bienvenue
 
-#include <iostream>
+#include "fonction-bienvenue.h"
 
 int main()
 {
-   std::cout <<"Bienvenue le monde !" << std::endl;
+   afficherBienvenue();
 
    return 0;
 }
